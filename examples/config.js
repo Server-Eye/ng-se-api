b9a10edbf008afad0ab69ad5d57cf8b7fd83e267
Kim Schneider <kim.schneider@kraemer-it.de>
2015-01-30 16:03:47 +0100

    set dummy api key

--- examples/config.js
+++ examples/config.js
@@ -1,5 +1,5 @@
 "use strict";
 
 angular.module('se').config(['sesApiConfigProvider', function(sesApiConfigProvider) {
-    sesApiConfigProvider.setApiKey('c3f505b0-a85c-11e4-8de9-cb82a6d17852');
+    sesApiConfigProvider.setApiKey('YOUR-SERVER-EYE-API-KEY');
 }]);
