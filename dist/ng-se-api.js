3934312fed95831d20194d7b3f80c820e36f7271
Kim Schneider <kim.schneider@kraemer-it.de>
2015-07-16 17:39:06 +0200

    added create report added delete report added list report types

--- dist/ng-se-api.js
+++ dist/ng-se-api.js
@@ -2327,27 +2327,89 @@
             var request = new SeaRequest('reporting/{cId}'),
                 reportRequest = new SeaRequest('reporting/{cId}/{rId}');
 
+            function formatReport(report) {
+                ['startDate', 'lastDate', 'nextDate'].forEach(function (prop) {
+                    if(report[prop]) {
+                        report[prop] = new Date(report[prop]);
+                    }
+                });
+                
+                if(report.history) {
+                    report.history.forEach(function (generated) {
+                        generated.generatedDate = new Date(generated.generatedDate);
+                    });
+                }
+                
+                return report;
+            }
+        
+            function create(params) {
+                return request.post(params);
+            }
+        
             function list(cId) {
                 return request.get({
                     cId: cId
+                }).then(function (reports) {
+                    reports.forEach(formatReport);
+                    return reports;
+                });
+            }
+        
+            function listTypes(cId) {
+                return reportRequest.get({
+                    cId: cId,
+                    rId: 'type'
                 });
             }
 
-            function listHistory(cId, rId) {
+            function get(cId, rId) {
                 return reportRequest.get({
+                    cId: cId,
+                    rId: rId
+                }).then(function (report) {
+                    return formatReport(report);
+                });
+            }
+        
+            function destroy(cId, rId) {
+                return reportRequest.del({
                     cId: cId,
                     rId: rId
                 });
             }
 
             return {
-                list: function () {
-                    return list();
+                list: function (cId) {
+                    return list(cId);
                 },
 
+                type: {
+                    list: function (cId) {
+                        return listTypes(cId);
+                    }
+                },
+                
                 report: {
-                    list: function (cId, rId) {
-                        return listHistory(cId, rId);
+                    get: function (cId, rId) {
+                        return get(cId, rId);
+                    },
+                    
+                    /**
+                     * create report
+                     * @param {Object} params
+                     * @config {String} [cId]
+                     * @config {String} [rtId]
+                     * @config {String} [targetId]
+                     * @config {String} [repeatInterval]
+                     * @config {String} [recipients]
+                     */
+                    create: function(params) {
+                        return create(params);
+                    },
+                    
+                    destroy: function (cId, rId) {
+                        return destroy(cId, rId);
                     }
                 }
             };
