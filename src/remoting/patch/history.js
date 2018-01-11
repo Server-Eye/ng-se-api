54f66524f093a304469282aa71ad7281915c80a1
Kim Schneider <kim.schneider@kraemer-it.de>
2018-01-11 09:34:11 +0100

    added more params to patch.history

--- src/remoting/patch/history.js
+++ src/remoting/patch/history.js
@@ -2,40 +2,53 @@
     "use strict";
 
     angular.module('ngSeApi').factory('seaRemotingPatchHistory', ['$http', 'SeaRequest', 'seaRemotingIasHelper',
-    function seaRemotingPcvisit($http, SeaRequest, helper) {
+        function seaRemotingPcvisit($http, SeaRequest, helper) {
             var request = new SeaRequest(helper.getUrl('seias/rest/seocc/patch/1.0/container/history/{action}'));
-        
+
             function format(container) {
-                if(!container.JobList) {
+                if (!container.JobList) {
                     return container;
                 }
-                
+
                 container.JobList.forEach(function (job) {
                     ['StartTime', 'EndTime', 'PlannedStartTime'].forEach(function (key) {
-                        if(job[key]) {
+                        if (job[key]) {
                             job[key] = new Date(job[key]);
                         }
                     });
                 });
-                
+
                 return container;
             }
-                
-            function get(customerId, cId, paging) {
-                return list(customerId, [cId], paging).then(function (history) {
+
+            function get(customerId, cId, params) {
+                return list(customerId, [cId], params).then(function (history) {
                     return (history[0] || {}).JobList;
                 });
             }
 
-            function list(customerId, containerIds, paging) {
+            function list(customerId, containerIds, params) {
                 var query = helper.getContainerIds(containerIds);
                 query.action = 'get';
-                
-                if(paging) {
-                    query.Index = paging.index;
-                    query.Count = paging.count;
+
+                params = params || {};
+
+                if (params.index != null) {
+                    query.Index = params.index;
                 }
-                
+
+                if (params.count != null) {
+                    query.Count = params.count;
+                }
+
+                if (params.from != null) {
+                    query.From = params.from;
+                }
+
+                if (params.states != null) {
+                    query.States = params.states;
+                }
+
                 return request.post(query).then(function (containers) {
                     containers.forEach(format);
                     return containers;
@@ -43,13 +56,13 @@
             }
 
             return {
-                get: function (customerId, cId, paging) {
-                    return get(customerId, cId, paging);
+                get: function (customerId, cId, params) {
+                    return get(customerId, cId, params);
                 },
 
-                list: function (customerId, containerIds, paging) {
-                    return list(customerId, containerIds, paging);
+                list: function (customerId, containerIds, params) {
+                    return list(customerId, containerIds, params);
                 }
             };
-    }]);
+        }]);
 })();
