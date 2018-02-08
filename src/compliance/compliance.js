2ff216ff237e733515dc0f2ff1e1555e61d8a3cc
M_Staudt <ms@aptex.de>
2018-02-08 11:09:29 +0100

    debugging compliance module

--- src/compliance/compliance.js
+++ src/compliance/compliance.js
@@ -4,24 +4,7 @@
     angular.module('ngSeApi').factory('seaCompliance', ['$q', 'SeaRequest', 'seaComplianceConfig', 'seaComplianceFix', 'seaComplianceViolation', 'seaRemotingIasHelper',
         function seaCompliance($q, SeaRequest, seaComplianceConfig, seaComplianceFix, seaComplianceViolation, helper) {
             function list(customerId, containerIds, tId, checks) {
-                return new Promise((resolve, reject) => {
-                    var violationList = [];
-                    var loopPromises = [];
-
-                    angular.forEach(containerIds, function (cId) {
-                        var deferred = $q.defer();
-                        loopPromises.push(deferred.promise);
-
-                        seaComplianceViolation.get(cId, tId, checks).then((res) => {
-                            violationList.push(res);
-                            deferred.resolve();
-                        });
-                    });
-
-                    $q.all(loopPromises).then(function () {
-                        resolve(violationList);
-                    });
-                });
+                return seaComplianceViolation.get(containerIds, tId, checks);
             }
 
             return {
