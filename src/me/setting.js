ab54b237028d61388db46bf5ce9c9578e3b7bcef
Martin Staudt <martin.staudt@server-eye.de>
2020-01-13 11:30:02 +0100

    added method to genereate key-pair

--- src/me/setting.js
+++ src/me/setting.js
@@ -4,6 +4,7 @@
     angular.module('ngSeApi').factory('seaMeSetting', ['SeaRequest',
     function seaMeSetting(SeaRequest) {
             var request = new SeaRequest('me/setting');
+            var requestAction = new SeaRequest('me/setting/{action}');
 
             function list() {
                 return request.get();
@@ -12,6 +13,13 @@
             function update(settings) {
                 settings = settings || {};
                 return request.put(settings);
+            }
+
+            function resetSecret(password) {
+                return requestAction.post({
+                    action: 'secret/reset',
+                    password: password,
+                });
             }
 
             return {
@@ -25,6 +33,12 @@
                  */
                 update: function (settings) {
                     return update(settings);
+                },
+
+                secret: {
+                    reset: function(password) {
+                        return resetSecret(password);
+                    }
                 }
             };
     }]);
